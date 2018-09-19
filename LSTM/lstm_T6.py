8ea5001f7c0117d625209c0ad15a81909e1b69ba
SiThuc <38562182+SiThuc@users.noreply.github.com>
2018-09-19 14:17:01 +0200

    Update lstm_T6.py

--- LSTM/lstm_T6.py
+++ LSTM/lstm_T6.py
@@ -243,34 +243,3 @@ from keras.callbacks import ModelCheckpoint
 filepath="model/T6_Dataset/lstm_T6_best_weights.{epoch:02d}-{val_acc:.4f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
 model.fit(x_train_seq, y_train, batch_size=64, epochs=5, validation_data=(x_val_seq, y_validation), callbacks = [checkpoint, metric])
-
-# model.fit(x_train_seq, y_train, batch_size = batch_size, nb_epoch = 5, verbose = 2, validation_data=(x_val_seq, y_validation))
-
-
-# from keras.layers import Input, Dense, concatenate, Activation, Conv1D, GlobalMaxPooling1D
-# from keras.models import Model
-#
-# tweet_input = Input(shape=(45,), dtype='int32')
-# tweet_encoder = Embedding(100000, 200, weights=[embedding_matrix], input_length=45, trainable=True)(tweet_input)
-# bigram_branch = Conv1D(filters=100, kernel_size=2, padding='valid', activation='relu', strides=1)(tweet_encoder)
-# bigram_branch = GlobalMaxPooling1D()(bigram_branch)
-# trigram_branch = Conv1D(filters=100, kernel_size=3, padding='valid', activation='relu', strides=1)(tweet_encoder)
-# trigram_branch = GlobalMaxPooling1D()(trigram_branch)
-# fourgram_branch = Conv1D(filters=100, kernel_size=4, padding='valid', activation='relu', strides=1)(tweet_encoder)
-# fourgram_branch = GlobalMaxPooling1D()(fourgram_branch)
-# merged = concatenate([bigram_branch, trigram_branch, fourgram_branch], axis=1)
-#
-# merged = Dense(256, activation='relu')(merged)
-# merged = Dropout(0.2)(merged)
-# merged = Dense(1)(merged)
-# output = Activation('sigmoid')(merged)
-# model = Model(inputs=[tweet_input], outputs=[output])
-# model.compile(loss='binary_crossentropy',
-#                   optimizer='adam',
-#                   metrics=['accuracy'])
-# model.summary()
-#
-# from keras.callbacks import ModelCheckpoint
-# filepath="model/T6_Dataset/CNN_best_weights.{epoch:02d}-{val_acc:.4f}.hdf5"
-# checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-# model.fit(x_train_seq, y_train, batch_size=64, epochs=5, validation_data=(x_val_seq, y_validation), callbacks = [checkpoint])
